7ca37217f36ed5ef0d6b506545b6f4ef3526a412
oushiming <oushiming@sxt.com>
2018-09-06 23:34:45 +0800

    init

---
+++ src/main/java/com/hmily/datastructuresdemo/DataStructuresDemoApplication.java
@@ -0,0 +1,12 @@
+package com.hmily.datastructuresdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataStructuresDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataStructuresDemoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/hmily/datastructuresdemo/DataStructuresDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hmily.datastructuresdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DataStructuresDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
