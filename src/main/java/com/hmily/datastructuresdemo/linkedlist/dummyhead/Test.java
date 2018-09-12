e49d1aac058d7f7800ed08b7f3bf5b7c91ecf172
oushiming <oushiming@sxt.com>
2018-09-13 00:27:17 +0800

    虚拟头链表

---
+++ src/main/java/com/hmily/datastructuresdemo/linkedlist/dummyhead/Test.java
@@ -0,0 +1,32 @@
+package com.hmily.datastructuresdemo.linkedlist.dummyhead;
+
+public class Test {
+
+    public static void main(String[] args) {
+        test1();
+
+    }
+
+    public static void test1(){
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        for(int i = 0 ; i < 5 ; i ++){
+            linkedList.addFirst(i);
+            System.out.println(linkedList);
+        }
+
+        linkedList.add(2, 666);
+        System.out.println(linkedList);
+
+        linkedList.set(2, 888);
+        System.out.println(linkedList);
+
+        linkedList.remove(2);
+        System.out.println(linkedList);
+
+        linkedList.removeFirst();
+        System.out.println(linkedList);
+
+        linkedList.removeLast();
+        System.out.println(linkedList);
+    }
+}
