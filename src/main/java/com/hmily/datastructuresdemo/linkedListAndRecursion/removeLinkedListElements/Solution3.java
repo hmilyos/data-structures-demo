4dd627d8dde7b6fb9791cbcce919c12a9e3b45a9
oushiming <oushiming@sxt.com>
2018-09-16 17:00:55 +0800

    补充测试代码

--- src/main/java/com/hmily/datastructuresdemo/linkedListAndRecursion/removeLinkedListElements/Solution3.java
+++ src/main/java/com/hmily/datastructuresdemo/linkedListAndRecursion/removeLinkedListElements/Solution3.java
@@ -21,4 +21,14 @@ public class Solution3 {
         }
         return dummyHead.next;
     }
+
+    public static void main(String[] args) {
+
+        int[] nums = {1, 2, 6, 3, 4, 5, 6};
+        ListNode head = new ListNode(nums);
+        System.out.println(head);
+
+        ListNode res = (new Solution3()).removeElements(head, 6);
+        System.out.println(res);
+    }
 }
